dea0127693a8805f7a145f20f2c3f8c5c64e2e0f
FRMarina <m.fedchikrolich@gmail.com>
2026-09-03 20:53:49 +0300

    feat: resolve-task1

--- task1.ts
+++ task1.ts
@@ -1,8 +1,23 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 const name: string = "Александр";
 
 const text: string = `Я, ${name}, учу TYPESCRIPT`;
 
 //Написать программу, которая выводит text в нижнем регистре с вашим именем, если имя не совпадает, то заменить его
 
-const yourName: string = "Настя";
+const yourName: string = "Marina";
+// Вариант 1
+/*
+if (text.includes(name === yourName)) {
+  console.log(text.toLowerCase());
+} else {
+  console.log(text.replace(name, yourName));
+}
+*/
+
+//Вариант 2
+if (text.includes(yourName)) {
+  console.log(text.toLowerCase());
+} else {
+  const newText: string = text.slice(0, text.indexOf(name)).concat(yourName).concat();
+  console.log(newText);
+}
